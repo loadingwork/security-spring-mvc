3188a7e828b8d0461363e432476bacfe15d9b868
irayslu <irayslu@163.com>
2019-03-12 21:53:44 +0800

    多数据源

---
+++ src/main/java/com/lw/mvc/controller/LoginController.java
@@ -0,0 +1,16 @@
+package com.lw.mvc.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * 登录控制器
+ * @author root
+ *
+ */
+@Controller
+public class LoginController {
+	
+	
+	
+
+}
